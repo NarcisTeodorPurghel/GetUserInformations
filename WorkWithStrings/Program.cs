878e8b2741c7b110b5ff68a9f33346f7e3255408
Narcis <narcis.teodor30@gmail.com>
2023-03-25 19:25:05 +0200

    Add second project.

--- WorkWithStrings/Program.cs
+++ WorkWithStrings/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UserBasicInformation
+namespace WorkWithStrings
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
         }
     }
 }
