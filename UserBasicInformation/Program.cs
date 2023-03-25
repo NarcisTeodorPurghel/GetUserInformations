80b3f6342556f2ca63367557635598be310cb68a
Narcis <narcis.teodor30@gmail.com>
2023-03-25 20:33:17 +0200

    Finished first program.

--- UserBasicInformation/Program.cs
+++ UserBasicInformation/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,47 @@ namespace UserBasicInformation
 {
     internal class Program
     {
+        public enum Gender
+        {
+            Male,
+            Female
+        }
         static void Main(string[] args)
         {
+            int year, month, day;
+            char gender;
 
+            Console.WriteLine("Enter your born year.");
+            year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter month.");
+            month = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter day.");
+            day = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Select your gender (M/F).");
+            gender = char.Parse(Console.ReadLine());
+
+            DateTime userDateOfBirth = new DateTime(year, month, day);
+            Console.WriteLine(userDateOfBirth.ToString("dd/MM/yyyy"));
+
+            string checkGender;
+
+            if (gender == 'M' || gender == 'm')
+            {
+                checkGender = Gender.Male.ToString();
+                Console.WriteLine("You will be retired at 65 years old.");
+            }
+            else if (gender == 'F' || gender == 'f')
+            {
+                checkGender = Gender.Female.ToString();
+                Console.WriteLine("You will be retired at 63 years old.");
+            }
+            else
+            {
+                checkGender = null;
+            }
         }
     }
 }
