b04d4e856a94bdb87ccc39e7ef24cd3be0b66209
Narcis <narcis.teodor30@gmail.com>
2023-03-25 20:46:34 +0200

    Add string methods.

--- WorkWithStrings/Program.cs
+++ WorkWithStrings/Program.cs
@@ -10,6 +10,15 @@ namespace WorkWithStrings
     {
         static void Main(string[] args)
         {
+            string greetings = "Happy to meet you,";
+            string name = " Narcis";
+            Console.WriteLine(string.Concat(greetings, name));
+            Console.WriteLine(string.IsNullOrEmpty(name));
+            Console.WriteLine(name.Replace("s", "T"));
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine(name.TrimStart(' '));
+            Console.WriteLine(name.Trim());
+            Console.WriteLine(name.IndexOf('N'));
         }
     }
 }
